a9ff4c4b29ac9c80a24fb630134e7901885a5ba9
soowan95 <ksw9565@gmail.com>
2026-05-19 23:40:32 +0900

    feat: init project

---
+++ cmd/localhost/deploy.sh
@@ -0,0 +1,4 @@
+docker-compose -f ../../docker-compose-localhost.yml up -d --build
+
+# docker-compose -f ../../docker-compose-localhost.yml up -d --build 'nestjs'
+# docker-compose -f ../../docker-compose-localhost.yml up -d --build 'mysql'
