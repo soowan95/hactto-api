89da6318d5ea75934ba4f6be88ee565e6177ed28
김수완 <138478877+soowan95@users.noreply.github.com>
2026-05-23 21:13:48 +0900

    feat: Add task for fetching recent winning number at every saturday 9pm (#6)

--- src/modules/winning-number/winning-number.service.ts
+++ src/modules/winning-number/winning-number.service.ts
@@ -25,62 +25,70 @@ export class WinningNumberService {
     }
 
     // The last 10 episodes, excluding the most recent one.
-    const response1 = await firstValueFrom(
+    const response = await firstValueFrom(
       this.httpService.get<Lt365ResponseDto>(
         `https://www.dhlottery.co.kr/lt645/selectPstLt645InfoNew.do?srchDir=older&srchCursorLtEpsd=${lastestEpisode}`,
       ),
     );
-    const lt365s: Lt365[] = response1.data.data.list;
+    const lt365s: Lt365[] = response.data.data.list;
     for (const lt365 of lt365s) {
       lt365Map.set(lt365.ltEpsd, lt365);
     }
 
-    // Most recent episode.
-    const response2 = await firstValueFrom(
+    const lt365List: Lt365[] = Array.from(lt365Map.values());
+
+    for (const lt365 of lt365List) {
+      await this.upsert(lt365);
+    }
+
+    await this.fetchRecentOne();
+  }
+
+  async fetchRecentOne(): Promise<void> {
+    const response = await firstValueFrom(
       this.httpService.get<Lt365ResponseDto>(
         `https://www.dhlottery.co.kr/lt645/selectPstLt645Info.do`,
       ),
     );
-    const lt365: Lt365 = response2.data.data.list.pop()!;
-    lt365Map.set(lt365.ltEpsd, lt365);
 
-    const lt365List: Lt365[] = Array.from(lt365Map.values());
+    const lt365: Lt365 = response.data.data.list.pop()!;
+    await this.upsert(lt365);
+  }
 
-    for (const lt365 of lt365List) {
-      await prisma.winningNumber.upsert({
-        where: { episode: lt365.ltEpsd },
-        update: {
-          first: lt365.tm1WnNo,
-          second: lt365.tm2WnNo,
-          third: lt365.tm3WnNo,
-          fourth: lt365.tm4WnNo,
-          fifth: lt365.tm5WnNo,
-          sixth: lt365.tm6WnNo,
-          bonus: lt365.bnsWnNo,
-        },
-        create: {
-          episode: lt365.ltEpsd,
-          first: lt365.tm1WnNo,
-          second: lt365.tm2WnNo,
-          third: lt365.tm3WnNo,
-          fourth: lt365.tm4WnNo,
-          fifth: lt365.tm5WnNo,
-          sixth: lt365.tm6WnNo,
-          bonus: lt365.bnsWnNo,
-        },
-      });
-    }
+  private async upsert(lt365: Lt365): Promise<void> {
+    await prisma.winningNumber.upsert({
+      where: { episode: lt365.ltEpsd },
+      update: {
+        first: lt365.tm1WnNo,
+        second: lt365.tm2WnNo,
+        third: lt365.tm3WnNo,
+        fourth: lt365.tm4WnNo,
+        fifth: lt365.tm5WnNo,
+        sixth: lt365.tm6WnNo,
+        bonus: lt365.bnsWnNo,
+      },
+      create: {
+        episode: lt365.ltEpsd,
+        first: lt365.tm1WnNo,
+        second: lt365.tm2WnNo,
+        third: lt365.tm3WnNo,
+        fourth: lt365.tm4WnNo,
+        fifth: lt365.tm5WnNo,
+        sixth: lt365.tm6WnNo,
+        bonus: lt365.bnsWnNo,
+      },
+    });
   }
 
   /**
-   * Find all winning numbers from WINNING_NUMBER table.
+   * Find all winning numbers from the WINNING_NUMBER table.
    */
   async findAll(): Promise<WinningNumber[]> {
     return prisma.winningNumber.findMany();
   }
 
   /**
-   * Find one winning number from WINNING_NUMBER table by episode.
+   * Find one winning number from the WINNING_NUMBER table by episode.
    */
   async findByEpisode(episode: number): Promise<WinningNumber | null> {
     return prisma.winningNumber.findUnique({
