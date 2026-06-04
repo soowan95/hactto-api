506667c1aa89331ba75da1b6f99394a40b28f56b
soowan95 <ksw9565@gmail.com>
2026-06-05 06:28:16 +0900

    feat: add comprehensive analytical metrics to lottery analysis

--- src/modules/lottery-analysis/domain/aggregates/analysis.entity.ts
+++ src/modules/lottery-analysis/domain/aggregates/analysis.entity.ts
@@ -3,6 +3,23 @@ import { ReliabilityScore } from '../vos/reliability-score.vo';
 export class DomainAnalysis {
   public readonly id?: number;
   public reliability: ReliabilityScore;
+  public readonly sum: number;
+  public readonly cnt0s: number;
+  public readonly cnt10s: number;
+  public readonly cnt20s: number;
+  public readonly cnt30s: number;
+  public readonly cnt40s: number;
+  public readonly sumLastDigits: number;
+  public readonly lastDigit0: string;
+  public readonly lastDigit1: string;
+  public readonly lastDigit2: string;
+  public readonly lastDigit3: string;
+  public readonly lastDigit4: string;
+  public readonly lastDigit5: string;
+  public readonly lastDigit6: string;
+  public readonly lastDigit7: string;
+  public readonly lastDigit8: string;
+  public readonly lastDigit9: string;
   public readonly even: number;
   public readonly odd: number;
   public readonly hot: number;
@@ -15,6 +32,23 @@ export class DomainAnalysis {
 
   constructor(
     reliability: number,
+    sum: number,
+    cnt0s: number,
+    cnt10s: number,
+    cnt20s: number,
+    cnt30s: number,
+    cnt40s: number,
+    sumLastDigits: number,
+    lastDigit0: number[],
+    lastDigit1: number[],
+    lastDigit2: number[],
+    lastDigit3: number[],
+    lastDigit4: number[],
+    lastDigit5: number[],
+    lastDigit6: number[],
+    lastDigit7: number[],
+    lastDigit8: number[],
+    lastDigit9: number[],
     even: number,
     odd: number,
     hot: number,
@@ -27,6 +61,23 @@ export class DomainAnalysis {
     id?: number,
   ) {
     this.reliability = new ReliabilityScore(reliability);
+    this.sum = sum;
+    this.cnt0s = cnt0s;
+    this.cnt10s = cnt10s;
+    this.cnt20s = cnt20s;
+    this.cnt30s = cnt30s;
+    this.cnt40s = cnt40s;
+    this.sumLastDigits = sumLastDigits;
+    this.lastDigit0 = JSON.stringify(lastDigit0);
+    this.lastDigit1 = JSON.stringify(lastDigit1);
+    this.lastDigit2 = JSON.stringify(lastDigit2);
+    this.lastDigit3 = JSON.stringify(lastDigit3);
+    this.lastDigit4 = JSON.stringify(lastDigit4);
+    this.lastDigit5 = JSON.stringify(lastDigit5);
+    this.lastDigit6 = JSON.stringify(lastDigit6);
+    this.lastDigit7 = JSON.stringify(lastDigit7);
+    this.lastDigit8 = JSON.stringify(lastDigit8);
+    this.lastDigit9 = JSON.stringify(lastDigit9);
     this.even = even;
     this.odd = odd;
     this.hot = hot;
@@ -43,6 +94,7 @@ export class DomainAnalysis {
     prediction: number[],
     temperatures?: Record<number, 'HOT' | 'WARM' | 'COLD'>,
   ): DomainAnalysis {
+    if (prediction.length === 7) prediction = prediction.slice(0, 6);
     const { even, odd } = this.countEvenOdd(prediction);
     const { low, high } = this.countLowHigh(prediction);
     const ac = this.calculateArithmeticComplexity(prediction);
@@ -63,6 +115,23 @@ export class DomainAnalysis {
 
     return new DomainAnalysis(
       0,
+      prediction.reduce((a, b) => a + b, 0),
+      this.countByMultiOfTen(prediction, 0),
+      this.countByMultiOfTen(prediction, 1),
+      this.countByMultiOfTen(prediction, 2),
+      this.countByMultiOfTen(prediction, 3),
+      this.countByMultiOfTen(prediction, 4),
+      this.sumOfLastDigits(prediction),
+      this.getLastDigitGroup(prediction, 0),
+      this.getLastDigitGroup(prediction, 1),
+      this.getLastDigitGroup(prediction, 2),
+      this.getLastDigitGroup(prediction, 3),
+      this.getLastDigitGroup(prediction, 4),
+      this.getLastDigitGroup(prediction, 5),
+      this.getLastDigitGroup(prediction, 6),
+      this.getLastDigitGroup(prediction, 7),
+      this.getLastDigitGroup(prediction, 8),
+      this.getLastDigitGroup(prediction, 9),
       even.length,
       odd.length,
       hot,
@@ -79,6 +148,23 @@ export class DomainAnalysis {
     return {
       id: 0,
       reliability: 0,
+      sum: 0,
+      cnt0s: 0,
+      cnt10s: 0,
+      cnt20s: 0,
+      cnt30s: 0,
+      cnt40s: 0,
+      sumLastDigits: 0,
+      lastDigit0: '{}',
+      lastDigit1: '{}',
+      lastDigit2: '{}',
+      lastDigit3: '{}',
+      lastDigit4: '{}',
+      lastDigit5: '{}',
+      lastDigit6: '{}',
+      lastDigit7: '{}',
+      lastDigit8: '{}',
+      lastDigit9: '{}',
       even: 0,
       odd: 0,
       hot: 0,
@@ -88,6 +174,7 @@ export class DomainAnalysis {
       high: 0,
       ac: 0,
       consecutive: '{}',
+      createdAt: new Date(),
     };
   }
 
@@ -97,6 +184,18 @@ export class DomainAnalysis {
 
   getReliability() {
     return this.reliability.getScore();
+  }
+
+  private static countByMultiOfTen(prediction: number[], multi: number) {
+    return prediction.filter((n) => Math.floor(n / 10) === multi).length;
+  }
+
+  private static sumOfLastDigits(prediction: number[]) {
+    return prediction.reduce((sum, n) => sum + (n % 10), 0);
+  }
+
+  private static getLastDigitGroup(prediction: number[], group: number) {
+    return prediction.filter((n) => n % 10 === group);
   }
 
   private static countEvenOdd(prediction: number[]) {
