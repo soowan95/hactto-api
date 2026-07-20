913a88041a2084723cab7bdc1cf469dd5e003d40
soowan95 <ksw9565@gmail.com>
2026-07-20 12:09:21 +0900

    fix: correct prisma client import path in fix-prod-db script

--- scripts/fix-prod-db.js
+++ scripts/fix-prod-db.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../src/generated/prisma');
 const prisma = new PrismaClient();
 
 async function main() {
